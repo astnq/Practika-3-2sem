96e45c41bbdfc53891eeec79ec9edabe4e44feb4
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:33:35 +0300

    Update 6zad.cpp

--- 6zad.cpp
+++ 6zad.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
-#include <numeric>
+#include <numeric> // Для функции gcd()
 
 using namespace std;
 
+// Функция проверяет, можно ли поровну разделить торт
 bool can_cut_cake(int people, int slices) {
-    return gcd(people, slices) == people;
+    return gcd(people, slices) == people; // Проверяем, делится ли общее число кусков на количество людей
 }
 
 int main() {
     int people, slices;
     cout << "Введите количество людей и кусочков: ";
     cin >> people >> slices;
+
     cout << (can_cut_cake(people, slices) ? "Можно разделить поровну!" : "Нельзя.") << endl;
     return 0;
 }
