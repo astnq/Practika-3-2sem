6cb8e1f30d9b5ca28800e5d7dad6df82792a51bb
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:36:28 +0300

    Update 9zad.cpp

--- 9zad.cpp
+++ 9zad.cpp
@@ -7,19 +7,22 @@ using namespace std;
 int main() {
     string text;
     cout << "Введите строку: ";
-    getline(cin, text);
+    getline(cin, text); // Считываем всю строку с пробелами
 
     string vowels = "aeiouAEIOUаеёиоуыэюяАЕЁИОУЫЭЮЯ";
     string consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZбвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ";
 
+    // Подсчёт гласных букв
     int vowel_count = count_if(text.begin(), text.end(), [&](char c) {
         return vowels.find(c) != string::npos;
     });
 
+    // Подсчёт согласных букв
     int consonant_count = count_if(text.begin(), text.end(), [&](char c) {
         return consonants.find(c) != string::npos;
     });
 
+    // Вывод результата
     cout << "Гласных букв: " << vowel_count << endl;
     cout << "Согласных букв: " << consonant_count << endl;
 
