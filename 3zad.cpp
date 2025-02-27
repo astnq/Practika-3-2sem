a3dee454f899bab7af1eab20715aa2606ed8d8e9
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:31:58 +0300

    Update 3zad.cpp

--- 3zad.cpp
+++ 3zad.cpp
@@ -3,15 +3,20 @@
 #include <random>
 #include <algorithm>
 
+using namespace std;
+
+// Функция для генерации случайного пароля
 string generate_password(int length) {
-    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
-    string password(length, ' ');
-    generate(password.begin(), password.end(), [&chars]() {
-        static random_device rd;
-        static mt19937 gen(rd());
-        static uniform_int_distribution<> dis(0, chars.size() - 1);
-        return chars[dis(gen)];
+    string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*"; 
+    string password(length, ' '); 
+    
+    generate(password.begin(), password.end(), [&chars]() { // Заполняем случайными символами
+        static random_device rd; // Источник случайности
+        static mt19937 gen(rd()); // Генератор случайных чисел
+        static uniform_int_distribution<> dis(0, chars.size() - 1); // Распределение индексов по chars
+        return chars[dis(gen)]; 
     });
+
     return password;
 }
 
@@ -19,6 +24,7 @@ int main() {
     int len;
     cout << "Введите длину пароля: ";
     cin >> len;
+    
     cout << "Сгенерированный пароль: " << generate_password(len) << endl;
     return 0;
 }
