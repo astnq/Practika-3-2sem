521169ecaa094d7269c037d9c4c9cba4eeb10cc7
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:32:28 +0300

    Update 4zad.cpp

--- 4zad.cpp
+++ 4zad.cpp
@@ -5,25 +5,29 @@
 
 using namespace std;
 
+// Функция для поиска наиболее часто встречающегося числа
 void most_frequent_number() {
     vector<int> nums;
     int n, x;
+
     cout << "Введите количество чисел: ";
     cin >> n;
+    
     cout << "Введите числа: ";
-    while (n--) {
+    while (n--) {  // Вводим n чисел и добавляем в вектор
         cin >> x;
         nums.push_back(x);
     }
 
-    unordered_map<int, int> freq;
+    unordered_map<int, int> freq; // Хеш-таблица для подсчёта частоты чисел
     for_each(nums.begin(), nums.end(), [&freq](int x) { freq[x]++; });
 
+    // Поиск числа с максимальной частотой
     auto max_elem = max_element(freq.begin(), freq.end(), [](const auto& a, const auto& b) {
         return a.second < b.second;
     });
 
-    cout << "Число " << max_elem->first << " встречается " << max_elem->second << " раз." << std::endl;
+    cout << "Число " << max_elem->first << " встречается " << max_elem->second << " раз." << endl;
 }
 
 int main() {
