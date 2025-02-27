e0d7d958524ca4fa627db95ad896e63e31607110
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:39:00 +0300

    Update 11zad.cpp

--- 11zad.cpp
+++ 11zad.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
 using namespace std;
 
 int main() {
-    system ("chcp 65001");
+    system("chcp 65001");
+
+    // Список фруктов
     vector<string> fruits = {"абрикос", "персик", "абрикос", "персик", "абрикос", "абрикос"};
 
+    // Подсчёт количества абрикосов и персиков в векторе
     int abricos = count(fruits.begin(), fruits.end(), "абрикос");
     int persiki = count(fruits.begin(), fruits.end(), "персик");
-    int total_fruits = fruits.size();
+    int total_fruits = fruits.size(); // Общее количество фруктов
 
+    
     cout << "Всего фруктов: " << total_fruits << endl;
     cout << "Количество абрикосов: " << abricos << endl;
     cout << "Количество персиков: " << persiki << endl;
