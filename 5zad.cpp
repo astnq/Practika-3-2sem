e6fc4fab7decdc8353c007f2c0026570a87cb0e3
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:33:02 +0300

    Update 5zad.cpp

--- 5zad.cpp
+++ 5zad.cpp
@@ -4,14 +4,16 @@
 
 using namespace std;
 
-bool is_palindrome(const std::string& s) {
-    return equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
+// Функция для проверки, является ли строка палиндромом
+bool is_palindrome(const string& s) {
+    return equal(s.begin(), s.begin() + s.size() / 2, s.rbegin()); // Сравниваем первую половину с обратной второй
 }
 
 int main() {
     string word;
     cout << "Введите слово: ";
     cin >> word;
+
     cout << (is_palindrome(word) ? "Палиндром!" : "Не палиндром.") << endl;
     return 0;
 }
