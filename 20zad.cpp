1dcb2a9c970bd7a85ffe4d3e41ab7a954f2b6cd5
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:49:58 +0300

    Update 20zad.cpp

--- 20zad.cpp
+++ 20zad.cpp
@@ -5,13 +5,16 @@
 using namespace std;
 
 int main() {
-    vector<pair<int, int>> small_cargo = {{10, 5}, {5, 3}, {8, 4}}; // вес, размер
+    // Векторы с парами значений (вес, размер) для мелких и крупных грузов
+    vector<pair<int, int>> small_cargo = {{10, 5}, {5, 3}, {8, 4}}; 
     vector<pair<int, int>> large_cargo = {{30, 15}, {25, 10}, {40, 20}};
 
+    // Объединяем два списка грузов в один
     vector<pair<int, int>> all_cargo(small_cargo.begin(), small_cargo.end());
     all_cargo.insert(all_cargo.end(), large_cargo.begin(), large_cargo.end());
 
-    sort(all_cargo.begin(), all_cargo.end(), [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
+    // Сортируем грузы по размеру (по возрастанию), при равных размерах - по весу (по возрастанию)
+    sort(all_cargo.begin(), all_cargo.end(), [](const pair<int, int>& a, const pair<int, int>& b) {
         return a.second == b.second ? a.first < b.first : a.second < b.second;
     });
 
