e9ac6fee3a14173f5c77bf97d43e3985f8fa8f3b
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:37:49 +0300

    Update 10zad.cpp

--- 10zad.cpp
+++ 10zad.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int main() {
-    vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; 
+    vector<int> even_numbers, odd_numbers; // Векторы для хранения чётных и нечётных чисел
 
-    vector<int> even_numbers, odd_numbers;
-
+    // Разделение чисел на чётные и нечётные с помощью for_each и лямбда-функции
     for_each(numbers.begin(), numbers.end(), [&](int x) {
         (x % 2 == 0 ? even_numbers : odd_numbers).push_back(x);
     });
 
     cout << "Чётные числа: ";
     for (int num : even_numbers) cout << num << " ";
-    
+
     cout << "\nНечётные числа: ";
     for (int num : odd_numbers) cout << num << " ";
 
