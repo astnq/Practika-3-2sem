cde9487f0716567c512ab6d4c1b868a9784980f2
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:42:50 +0300

    Update 14zad.cpp

--- 14zad.cpp
+++ 14zad.cpp
@@ -5,11 +5,12 @@
 using namespace std;
 
 int main() {
-    vector<int> scores = {80, 70, 90, 60, 100};
+    vector<int> scores = {80, 70, 90, 60, 100}; // Исходные баллы студентов
 
+    // Находим максимальный балл, но сравниваем их, уменьшая в 2 раза
     double max_score = *max_element(scores.begin(), scores.end(), [](int a, int b) {
-        return a * 0.5 < b * 0.5;
-    }) * 0.5;
+        return a * 0.5 < b * 0.5; // Сравнение половинных значений
+    }) * 0.5; // Финальный результат тоже уменьшаем в 2 раза
 
     cout << "Максимальный конечный балл: " << max_score << endl;
 
