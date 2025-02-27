204a6cf7f96b686bc1abc5fce0cf027159cdfb03
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:31:08 +0300

    Update 2zad.cpp

--- 2zad.cpp
+++ 2zad.cpp
@@ -7,13 +7,14 @@ using namespace std;
 int main() {
     string sentence;
     cout << "Введите предложение: ";
-    getline(cin, sentence);
+    getline(cin, sentence);  // Считываем всю строку
 
-    istringstream iss(sentence);
-    vector<std::string> words{std::istream_iterator<string>{iss}, {}};
+    istringstream iss(sentence);  // Создаём поток для разделения строки
+    vector<string> words{istream_iterator<string>{iss}, {}};  // Разбиваем на слова
 
-    reverse(words.begin(), words.end());
+    reverse(words.begin(), words.end());  // Разворачиваем порядок слов
     
+    // Выводим слова в обратном порядке
     for (const auto& w : words) cout << w << " ";
     return 0;
 }
