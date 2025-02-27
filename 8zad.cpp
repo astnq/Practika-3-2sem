78577da7982a1eb9d8986f256478552be6e83d63
ksanax <163411035+astnq@users.noreply.github.com>
2025-02-27 21:35:16 +0300

    Update 8zad.cpp

--- 8zad.cpp
+++ 8zad.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-bool is_anagram(std::string s1, std::string s2) {
-    sort(s1.begin(), s1.end());
-    sort(s2.begin(), s2.end());
-    return s1 == s2;
+// Функция проверки анаграмм (слова, составленные из одних и тех же букв)
+bool is_anagram(string s1, string s2) {
+    sort(s1.begin(), s1.end()); // Сорт-ка первой строки
+    sort(s2.begin(), s2.end()); // Сорт-ка второй строки
+    return s1 == s2; // Если отсортированные строки совпадают — это анаграмма
 }
 
 int main() {
